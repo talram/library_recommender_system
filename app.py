3ac634b4744bd972a19cb63ada3799d3264c3824
Tal Ramon <tal.ramon93@gmail.com>
2021-01-05 20:04:50 +0200

    check

--- app.py
+++ app.py
@@ -2,45 +2,20 @@ import flask
 from flask_cors import CORS
 from flask import request
 import json
-#from Recommender import recommend
+from recommend_system import recommend_book
 
 app = flask.Flask(__name__)
 CORS(app)
 
 
-books = [
-    {'id': 0,
-     'title': 'A Fire Upon the Deep',
-     'author': 'Vernor Vinge',
-     'first_sentence': 'The coldsleep itself was dreamless.',
-     'year_published': '1992'},
-    {'id': 1,
-     'title': 'The Ones Who Walk Away From Omelas',
-     'author': 'Ursula K. Le Guin',
-     'first_sentence': 'With a clamor of bells that set the swallows soaring, the Festival of Summer came to the city Omelas, bright-towered by the sea.',
-     'published': '1973'},
-    {'id': 2,
-     'title': 'Dhalgren',
-     'author': 'Samuel R. Delany',
-     'first_sentence': 'to wound the autumnal city.',
-     'published': '1975'}
-]
-
-
-@app.route('/', methods=['GET'])
-def home():
-    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction " \
-           "novels.</p> "
-
-
-# A route to return all of the available entries in our catalog.
-@app.route('/books', methods=['POST'])
+# A route to return all of the recommended books to the shelf.
+@app.route('/recommender', methods=['POST'])
 def get_books():
-    #request_params = request.json()
-    result = ['test']#recommend(request_params['title'])
+    request_params = request.json()
+    result = recommend_book(request_params['name'])
 
     return json.dumps(result)
 
-if __name__ == '__main__':
-    app.run(port= 8080)
 
+if __name__ == '__main__':
+    app.run()
---
+++ recommend_system.py
@@ -0,0 +1,37 @@
+import pandas as pd
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import linear_kernel
+from ast import literal_eval
+
+
+books = pd.read_csv('name_books.csv', encoding='latin-1')
+
+books['isbn13'] = books['isbn13'].astype('int')
+books_tfidf = TfidfVectorizer(stop_words='english')
+
+books['title'] = books['title'].fillna('')
+book_title_matrix = books_tfidf.fit_transform(books['title'])
+
+similarity_matrix = cosine_similarity(book_title_matrix, book_title_matrix)
+
+mapping = pd.Series(books.index, index=books['title'])
+
+
+def recommend_book(book_input):
+    book_index = mapping[book_input]
+
+    # get similarity values with other books and similarity matrix
+    similarity_score = list(enumerate(similarity_matrix[book_index]))
+
+    # sort in descending order the similarity score of book inputted with all the other books
+    similarity_score = sorted(similarity_score, key=lambda x: x[1], reverse=True)
+
+    # get the scores of the 5 most similar books. Ignore the first book.
+    similarity_score = similarity_score[1:5]
+
+    # return books ISBN13 using the mapping series
+    book_indices = [i[0] for i in similarity_score]
+    return books['isbn13'].iloc[book_indices]
+
+
